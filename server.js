2ba3ffc9ccaab672d75edaa4e85c53591eb6f644
ProgecktProf <sergey.nikishin555123123@gmail.com>
2025-11-26 03:35:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ import { v4 as uuidv4 } from 'uuid';
 import sharp from 'sharp';
 import os from 'os';
 import sqlite3 from 'sqlite3';
+import { dbService } from './database.js';
 
 dotenv.config();
 
@@ -219,7 +220,6 @@ class RealDatabaseService {
     }
 }
 
-const dbService = new RealDatabaseService();
 // ==================== СИСТЕМА АУТЕНТИФИКАЦИИ И СЕССИЙ ====================
 class AuthService {
     static generateSessionToken() {
